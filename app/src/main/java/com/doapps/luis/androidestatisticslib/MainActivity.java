9f1a98cf9642c1630ba1b7e51b96f396a060cc0f
bsoto <bryam.soto@unmsm.edu.pe>
2016-07-12 16:30:39 -0500

    MALOGRANDO EL CODIGO

--- app/src/main/java/com/doapps/luis/androidestatisticslib/MainActivity.java
+++ app/src/main/java/com/doapps/luis/androidestatisticslib/MainActivity.java
@@ -9,8 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+		/*ACA MALOGRANDO EL CODIGO*/
     }
 
     private void init(){
