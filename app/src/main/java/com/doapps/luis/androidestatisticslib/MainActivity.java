fb6575a1e746a72e9c162a05769265592a82edac
bsoto <bryam.soto@unmsm.edu.pe>
2016-07-12 16:34:58 -0500

    ARREGLANDO EL CODIGO

--- app/src/main/java/com/doapps/luis/androidestatisticslib/MainActivity.java
+++ app/src/main/java/com/doapps/luis/androidestatisticslib/MainActivity.java
@@ -9,7 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		/*ACA MALOGRANDO EL CODIGO*/
+		
+		/*ARREGLANDO EL CODIGO*/
     }
 
     private void init(){
